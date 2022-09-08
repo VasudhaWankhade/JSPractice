9cea77d2d05260981c8cb6251756f911e042c446
harshal34480 <vashudhawankhade@gmail.com>
2022-09-08 22:03:33 +0530

    variable declaration and function

---
+++ 1.js
@@ -0,0 +1,26 @@
+// variable declaration
+
+let a = 10
+a = 20
+console.log(a)
+
+const b = 100
+    // b = 10 can not update value
+console.log(b)
+
+var c = "Vasudha"
+console.log(c)
+
+// function ==> to avoid code repeatition
+
+// calculator
+
+function calculator(x, y) {
+    console.log(x + y)
+    console.log(x - y)
+    console.log(x * y)
+    console.log(x / y)
+    console.log(x % y)
+
+}
+calculator(100, 20)
